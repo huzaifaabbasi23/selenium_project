b82c9c89924fca403b76e8e57946958593fa35e7
huzaifaabbasi23 <huzaifaabbasi01001@gmail.com>
2026-07-24 00:32:41 +0500

    Update python.py

--- python.py
+++ python.py
@@ -15,21 +15,3 @@ options = select.options
 print("Total languages:", len(options))
 
 driver.quit()
-
-# Password
-#mypassword = driver.find_element(By.ID, "password")
-#mypassword.send_keys("secret_sauce")
-#time.sleep(3)
-
-
-# Login Button
-#mylogin = driver.find_element(By.ID, "login-button")
-#mylogin.click()
-#driver.implicitly_wait(5)
-#time.sleep(5)
-
-#my_code=driver.find_element(By.CLASS_NAME,"product_sort_container")
-#my_code.send_keys("Price(low to high)")
-#time.sleep(10)
-# Close Browser
-driver.quit()
